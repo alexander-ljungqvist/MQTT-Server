9ebbf2d4093ea6dc2c4d7eb3d7dac910d30a8c66
Alexander Ljungqvist <al.ljungqvist@gmail.com>
2018-09-10 15:46:45 +0200

    dx

--- mqtt_server.js
+++ mqtt_server.js
@@ -28,6 +28,8 @@ app.get('/mqtt_response', cors(), function(req, res){
 var server = app.listen(8081, function () {
 
    var host = server.address().address
+   console.log(host);
    var port = server.address().port
+   console.log(port);
 
 })
